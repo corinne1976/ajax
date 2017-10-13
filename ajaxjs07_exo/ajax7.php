2c5aba2eb3a04492acd352913d89193b376c35e6
corinne1976 <corinne.tina@lepoles.com>
2017-10-13 16:47:30 +0200

    commit 8
    fin exercices ajax

--- ajaxjs07_exo/ajax7.php
+++ ajaxjs07_exo/ajax7.php
@@ -1,10 +1,12 @@
 <?php
-// faire le traitement pour afficher dans un tableau le nom guillaume 
+// faire le traitement pour afficher dans un tableau le nom guillaume
 require_once("init.inc.php");
 
 $tab = array();
 $tab['monresultat'] = '';
-
+if (isset($_POST['personne'])) {
+  $pdo->exec("INSERT INTO employes (prenom,sexe) VALUES ('$_POST[personne]','f')");
+}
 $resultat = $pdo -> query("SELECT * FROM employes");
 
 $tab['monresultat'] .= '<table border="10">';
@@ -12,28 +14,28 @@ $tab['monresultat'] .= '<table border="10">';
 $tab['monresultat'] .= '<tr>';
 
 for($i = 0; $i < $resultat->columnCount(); $i++ ){// compter le nombre de collones
-  $colonne = $resultat->getColumnMeta($i);// permet afficher les information de chaque colonne 
+  $colonne = $resultat->getColumnMeta($i);// permet afficher les information de chaque colonne
   $tab['monresultat'] .= '<th>' . $colonne['name'] . '</th>';
 }
 
 while($enregistrement = $resultat -> fetch(PDO::FETCH_ASSOC)){// tant qu'il a un employe dans la base de donne il va faire un faire traitement permet l'affichage de l'employe
-  
+
 
     $tab['monresultat'] .= '</tr>';
     foreach($enregistrement as $valeur){
       $tab['monresultat'] .= '<td>' . $valeur . '</td>';
-      
+
     }
     $tab['monresultat'] .= '</tr>';
-  
+
 }
 
 //echo $tab['monresultat'];
 
 // $f=fopen('test.txt', 'a');
 // fwrite($f, json_encode($tab));
-echo json_encode($tab);// json_encode permet de transformer le tableau array au bon format 
-// ce format (json) assure la possibilité de transporter les données , sans json nous ne pouvons pas transporteer les données 
+echo json_encode($tab);// json_encode permet de transformer le tableau array au bon format
+// ce format (json) assure la possibilité de transporter les données , sans json nous ne pouvons pas transporteer les données
 
 
 ?>
--- ajaxjs07_exo/index.php
+++ ajaxjs07_exo/index.php
@@ -3,15 +3,14 @@
 <html>
 <head>
   <script src='ajax.js'></script>
-  
+
 </head>
 <body>
   <div id="resultat">
   </div>
-  <form  action="#" method="post">
+  <form  action="#" method="post"><br>
     <input type="text" id="personne">
-    <button type="button" id="button"></button>
+    <button type="button" id="submit">envoyer</button>
   </form>
 </body>
 </html>
-
